e53c5e65afc7e52333c7b26fb94352cbe3c305b8
Today <76273733+today2098@users.noreply.github.com>
2024-12-22 19:39:26 +0900

    [update] Create a new struct, "H" for "Body" like map

--- body.go
+++ body.go
@@ -7,3 +7,22 @@ type Body interface {
 	// Copy returns the copy (api used in apperr.Error.Wrap funtion).
 	Copy() Body
 }
+
+// H represents Body like map.
+type H map[string]any
+
+var _ (Body) = (H)(nil)
+
+// Is reports true anytime (api called in apperr.Error.Is function).
+func (h H) Is(_ Body) bool {
+	return true
+}
+
+// Copy creates the copy (api used in apperr.Error.Wrap funtion).
+func (h H) Copy() Body {
+	clone := make(H, len(h))
+	for key, elem := range h {
+		clone[key] = elem
+	}
+	return clone
+}
